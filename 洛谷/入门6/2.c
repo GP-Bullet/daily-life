f19e51d6c7706cd528ca519851aa0894b4a81efb
GP-Bullet <1285719445@qq.com>
2021-12-25 10:25:34 +0800

    12.25

--- 洛谷/入门6/2.c
+++ 洛谷/入门6/2.c
@@ -1,33 +1,47 @@
 //质数筛
+//80
 #include<stdio.h>
-int zhishu(int a[0]); 
+#include<math.h>
+
+//
+int zhishu(int n);
 
 int main(){
-    int n,i;
+    int n,i,j=0;
+    int sum=0;
     scanf("%d",&n);
-    int a[n];
-    int b[n];
+    int a[101];
+    int b[101];
     for(i=0;i<n;i++){
         scanf("%d",&a[i]);
     }
-
-    //判断质数
     for(i=0;i<n;i++){
-        if(!(zhishu(a[i]))){
-        a[i]=a[i+1];
+        if(zhishu(a[i])){
+            b[j++]=a[i];
+            sum++;
+        }
+
+    }
+    
+    for(i=0;i<sum;i++){
+        printf("%d ",b[i]);
+    }
+
+    return 0;
+}
+
+int zhishu(int n){
+    if(n==2||n==3){
+        return 1;
+    }
+    if(n%6!=1 && n%6!=5){
+        return 0;
+    }
+    for(int i=5;i<=sqrt(n);i+=6){
+        if(n%i==0||n%(i+2)==0){
+            return 0;
         }
     }
 
-
-    return 0;
-
-}
-
-int zhishu(int a[0]){
-    int i;
-    for(i=2;i<=a[i]-1;i++){
-        if(a[i]%i==0)
-        return 0;
-    }
     return 1;
 }
