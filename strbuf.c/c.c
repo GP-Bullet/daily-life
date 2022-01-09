352ea7ce6d70f5c704d02927900389414d825599
GP-Bullet <1285719445@qq.com>
2022-01-09 11:39:02 +0800

    1.9

--- strbuf.c/c.c
+++ strbuf.c/c.c
@@ -8,13 +8,49 @@ struct strbuf{
     char *buf;
 };
 
-void strbuf_rtrim(struct strbuf *sb);//左端是哪?
+void strbuf_rtrim(struct strbuf *sb);//左端是哪?->最开始的左端 还是  从左端开始
 void strbuf_ltrim(struct strbuf *sb);
 void strbuf_remove(struct strbuf *sb, size_t pos, size_t len);
 
 
-
+/*
 void strbuf_rtrim(struct strbuf*sb){
     int i=0;
-    while(sb->buf[i]==" ")
+    while(sb->buf[i]=='\0'){
+        if(sb->buf[i]==' '){
+            strcpy(sb->buf+i,"");
+            sb->len--;
+        }
+        i++;
+    }
+
+}*/
+//*这俩函数名字是反的
+void strbuf_rtrim(struct strbuf*sb){
+    int i=0;
+    while(sb->buf[i]==' '){
+        sb->len--;
+        i++;
+    }
+    char*ptr=(char*)malloc(sizeof(char)*(sb->len+1));
+    strcpy(ptr,sb->buf+i);
+    fflush(stdin);
+    strcpy(sb->buf,ptr);
 }
+
+void strbuf_ltrim(struct strbuf*sb){
+    int i=sb->len-1;
+    while(sb->buf[i]==' '){
+        sb->len--;
+        sb->buf[i]=='\0';
+        i--;
+    }
+}
+void strbuf_remove(struct strbuf *sb, size_t pos, size_t len){
+    if(pos<=sb->len&&len<=sb->len-pos){
+        strcpy(sb->buf+pos,sb->buf+pos+len);
+    }else{
+        printf("the pos or len is wrong!");
+    }
+}
+
