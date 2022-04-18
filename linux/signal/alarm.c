6883d0c8b00b11980e4626e11845aee6698e582b
GP-Bullet <1285719445@qq.com>
2022-04-18 09:20:24 +0800

    signal

---
+++ linux/signal/alarm.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<unistd.h>
+int main(void){
+    int counter;
+    alarm(1);
+    for(counter=0;1;counter++){
+        printf("counter=%d ",counter);
+    }
+    return 0;
+}
