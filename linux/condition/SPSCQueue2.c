eb1ab6c48553d437dc8fef6873d913424fd45c41
GP-Bullet <1285719445@qq.com>
2022-04-28 00:05:34 +0800

    4.28

--- linux/condition/SPSCQueue2.c
+++ linux/condition/SPSCQueue2.c
@@ -21,12 +21,13 @@ struct SPSCQueue{
 }typedef SPSCQueue;
 
 
-SPSCQueue *SPSCQueueInit(int threadNumber);
+SPSCQueue *SPSCQueueInit();
 void SPSCQueuePush(SPSCQueue *pool, void *s);
 void *SPSCQueuePop(SPSCQueue *pool);
 void SPSCQueueDestory(SPSCQueue *);
 
-SPSCQueue *SPSCQueueInit(int threadNumber){
+SPSCQueue *SPSCQueueInit()
+{
     SPSCQueue *Q;
     Q->front=0;
     Q->rear=0;
@@ -34,16 +35,72 @@ SPSCQueue *SPSCQueueInit(int threadNumber){
     return Q;
 }
 
-void SPSCQueuePush(SPSCQueue *pool, void *s){
+void SPSCQueuePush(SPSCQueue *pool, void *s)
+{
+    for(;;){
+
+        pool=malloc(sizeof(SPSCQueue));
+        
+        pthread_mutex_lock(&lock);
+        pool->data[pool->rear]=(int) *s;
+        pool->rear=(pool->rear+1)%MAXSIZE;
+        pthread_mutex_unlock(&lock);
+
+        pthread_cond_signal(&has_product);//将等待在该条件变量上的一个线程唤醒
+        sleep(rand()%5);
+    
+    }
+
+
+    if((pool->rear+1)%MAXSIZE==pool->front){//队列满
+        ;
+    }
+    
     
 }
 
-void *SPSCQueuePop(SPSCQueue *pool){
+void *SPSCQueuePop(SPSCQueue *pool)
+{   
+    for(;;){
+        pthread_mutex_lock(&lock);
+        while(pool->front==pool->rear){//队列是空的
+            pthread_cond_wait(&has_product,&lock);
+        }
+    }
+    //消费一个数据
+    pool->front=(pool->front+1)%MAXSIZE;
+    pthread_mutex_unlock(&lock);
+    printf("-Consume %lu ---%d\n",pthread_self(),pool->data);
+    free(pool);
+    sleep(rand()%5);        //?
 
 }
 
 
-int main(){
-    pthread_t pid,cid;
 
+
+
+
+
+int main(){
+    struct Param{
+        SPSCQueue *pool;
+        void *s;
+    }
+    Param q;
+    
+    int ret;
+    pthread_t pid,cid;
+    SPSCQueueInit();
+
+    ret=pthread_create(&pid,NULL,SPSCQueuePush,&);
+    if(ret!=0);;
+    
+    ret=pthread_create(&cid,NULL,SPSCQueuePop,&);
+    if(ret!=0);;
+
+    pthread_join(pid,NULL);
+    pthread_join(cid,NULL);
+
+    return 0;
 }
