d382cc5ce9448a4fed8035bb237605cffffc9919
GP-Bullet <1285719445@qq.com>
2022-05-24 00:38:42 +0800

    5.24

--- cpp/array.cpp
+++ cpp/array.cpp
@@ -7,4 +7,20 @@ string strs[get_size()];
 
 !！！！！！！！！怎么深挖编译器
 
+
+理解复杂的数组声明
+
+使用数组的时候编译器会把它转换成指针
+
+#Include<iterator>
+
+类比迭代器 
+    int ia[]={0,1,2,3,4,5,6,7,8,9};
+    int *beg=begin(ia);
+    int *last=end(ia);
+
+!内置下标运算可以为负值
+
+
+C风格字符串不要用了
 */
