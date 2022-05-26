8181f71f5bde9f63f7b92574873676b843f34be2
GP-Bullet <1285719445@qq.com>
2022-05-26 23:00:04 +0800

    this

---
+++ cpp/to the object/object/NULL.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+/*
+空指针调用成员函数  可以访问函数
+m_age 默认会有 this->age  当前对象的指针  空的值无法访问属性
+*/
+class Person
+{
+
+public:
+    void showClassName()
+    {
+
+    }
+};
+
+
+int main(){
+
+}
--- cpp/to the object/object/this.cpp
+++ cpp/to the object/object/this.cpp
@@ -8,21 +8,36 @@ using namespace std;
 成员变量和成员函数分开存储
 !只有非静态成员 属于类的对象上
 
-this指针 
 
+this指针 
+解决名称冲突
+返回对象本身
+隐含在每个成员函数内部
 */
 
 class Person{
-    int m_A;//
+public:
+    Person(int age){
+        //this指针指向的是   被调用的成员函数   所属的对象  this->  p1->age
+        this->age=age;
+    }
+    int age;
+    Person& Personadd(Person &p1){//返回若用的是引用      返回若用值创建一个新的对象 
+        this->age=p1.age;
+        return *this;
+        //链式编程
+    }
 };
 void test01(){
 
 }
+void test02(){
+
+}
 int main(){
-    Person a;
+    Person p1(10);
 
-    //!空对象占用一个字节   C++编译器为了区分空对象占内存的位置
-
+    //!空对象占用一个字节   C++编译器为了区分空对象占内存的位置、
 
     return 0;
 }
